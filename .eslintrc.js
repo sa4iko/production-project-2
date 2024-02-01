1c246f0868e796603ad4eec40e073c30872d058d
sergienko <sergienko@cortona3d.com>
2024-02-01 17:15:14 +0300

    add jest, tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,10 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -31,19 +34,18 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', {
-            markupOnly: true,
-            ignoreAttribute: ['data-testid', 'to'],
-        }],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true }],
+
     },
     globals: {
         __IS_DEV__: true,
     },
-    overrides: [{
-        files: '**/src/**/*.{test,stories}.{ts,tsx}',
-        rules: {
-            'i18next/no-literal-string': 'off',
-            'max-len': 'off',
-        },
-    }],
+    // overrides: [{
+    //     files: '**/src/**/*.{test,stories}.{ts,tsx}',
+    //     rules: {
+    //         'i18next/no-literal-string': 'off',
+    //         'max-len': 'off',
+    //     },
+    // }],
 };
